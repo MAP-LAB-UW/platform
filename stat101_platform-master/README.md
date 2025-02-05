4e851252181276cd6452811f9f1e2620ddadca7e
joshwilliams123 <jaw67@uw.edu>
2025-02-04 19:23:11 -0800

    front end designed and pages linked

--- stat101_platform-master/README.md
+++
@@ -1,3 +0,0 @@
-# stat101_platform
-
-This is the repository for the stat101 research project
--- stat101_platform-master/stats-college-research-project/stat101/resources/js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import studentTestReducer from './studentTestReducer';
-
-export default combineReducers({
-    auth: authReducer,
-    takeTest: studentTestReducer
-});
